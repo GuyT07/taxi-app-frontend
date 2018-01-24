960451b256d0f04210bb10ae233b353d3e1366f2
Guy Theuws <guy.theuws@luminis.eu>
2018-01-24 15:41:23 +0100

    List component

---
+++ src/components/List.vue
@@ -0,0 +1,22 @@
+<template>
+  <div ="hello">
+    List
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+export default {
+  name: 'List',
+  data () {
+    return {
+      msg: 'Welcome to List'
+    }
+  },
+  methods: mapActions([
+    'increment',
+    'decrement'
+  ])
+}
+</script>
+
